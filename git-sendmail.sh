03b19abb229896045a0834e5938610d0e1c64544
Eli Qiao <taget@linux.vnet.ibm.com>
2012-07-19 13:01:12 +0800

    Add git-sendmail.sh
    Signed-off-by: Eli Qiao <taget@linux.vnet.ibm.com>

--- git-sendmail.sh
+++ git-sendmail.sh
@@ -2,8 +2,10 @@
 
 echo $@
 
-git send-email --smtp-server ap.relay.ibm.com "$@" --from taget@linux.vnet.ibm.com --to frobisher@lists.linux.ibm.com
+#git send-email --smtp-server ap.relay.ibm.com "$@" --from taget@linux.vnet.ibm.com --to frobisher@lists.linux.ibm.com
 
 
-#git send-email --smtp-server ap.relay.ibm.com "$@" --from taget@linux.vnet.ibm.com --to taget@linux.vnet.ibm.com
+git send-email --smtp-server ap.relay.ibm.com "$@" --from taget@linux.vnet.ibm.com --to taget@linux.vnet.ibm.com
 
+
+#git send-email --smtp-server ap.relay.ibm.com "$1" --from taget@linux.vnet.ibm.com --to rhevh-blue@lists.linux.ibm.com
