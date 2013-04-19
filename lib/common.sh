2a397a131f32a35ad086fd45895072ba8857a6b7
Eli Qiao <taget@linux.vnet.ibm.com>
2013-04-19 10:24:32 +0800

    add _get_field
    Signed-off-by: Eli Qiao <taget@linux.vnet.ibm.com>

--- lib/common.sh
+++ lib/common.sh
@@ -5,11 +5,26 @@ _basename="/bin/basename"
 
 
 #get the real path of a file
+#_get_real_path /home/taget
+#return /home/taget
 function _get_real_path()
 {
-    PATH=$1
-    REAL_PATH=$(cd "$(${_dirname} ${PATH})" && pwd)/$(${_basename} ${PATH})
+    local PATH=$1
+    local REAL_PATH=$(cd "$(${_dirname} ${PATH})" && pwd)/$(${_basename} ${PATH})
     echo ${REAL_PATH}
     return 0
+}
+
+#get 
+#_get_field "a-b-cc-d" 2 "-"
+#return b
+function _get_field
+{
+   local text=$1
+   local pos=$2
+   local delimiter=$3
+   local field=$(echo ${text} | cut -d ${delimiter} -f${pos})
+   echo ${field}
+   return 0
 }
 
--- lib/test.sh
+++ lib/test.sh
@@ -8,3 +8,5 @@ set -x
 realpath=$(_get_real_path $0)
 echo ${realpath}
 
+filed=$(_get_field "a-b-cc-d" 3 "-")
+echo ${filed}
