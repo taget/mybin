95c888af81a2aa10bd956c0f5059aeb4da32ea58
Eli Qiao <taget@linux.vnet.ibm.com>
2013-04-28 14:40:01 +0800

    add test remove line number

--- lib/test.sh
+++ lib/test.sh
@@ -3,6 +3,7 @@
 set -x
 
 . ./common.sh
+. ./sed_command.sh
 
 # test real path
 realpath=$(_get_real_path $0)
@@ -10,3 +11,6 @@ echo ${realpath}
 
 filed=$(_get_field "a-b-cc-d" 3 "-")
 echo ${filed}
+
+# test remove line number
+remove_line_number ./tmp
