658409e99980b3e77176fe21da35412bd83be919
Eli Qiao <taget@linux.vnet.ibm.com>
2013-05-09 11:29:15 +0800

    Modify pno.sh, add parameter
    Signed-off-by: Eli Qiao <taget@linux.vnet.ibm.com>

--- pno.sh
+++ pno.sh
@@ -1,20 +1,105 @@
 #!/bin/bash
 
 APPLIST=(
-	#"ibm-notes8"
+	"notes"
 	"thunderbird"
 	"pidgin"
 	"goldendict"
+        "qq"
 )
-ps -ef | grep notes | grep -v grep | awk '{print $2}' | xargs kill -9 
-/usr/bin/ibm-notes8 &
-ps -ef | grep qq | grep -v grep | awk '{print $2}' | xargs kill -9
-/home/qiaoliyong/Downloads/linuxqq_v1.0.2_i386/run &
-for app in "${APPLIST[@]}"; do
-	ps -ef | grep $app | grep -v grep | awk '{print $2}'| xargs kill -9	
-	/usr/bin/$app &
-	echo "$app done!"
+
+usage() {
+cat << EOF
+usage: $0 [options]
+
+    -s Show the apps run list.
+    -l List the app lists.
+    -r Restart all apps.
+    -k Kill all apps.
+    -m Maintain all apps.
+Example:
+$0 
+EOF
+}
+# kill $1
+function kill_bin()
+{ 
+  APP=$1
+  ps -ef | grep ${APP} | grep -v grep | awk '{print $2}' | xargs kill -9 >/dev/null 2&1
+  return $?
+}
+
+function run_bin()
+{
+  APP=$1
+  /usr/bin/${APP} &
+  return $?
+}
+
+function show_app()
+{
+  echo "App Running list .... "
+  for app in "${APPLIST[@]}"; do
+    ps -ef | grep ${app} | grep -v "grep" >/dev/null 2>&1
+    if [[ $? -eq 0 ]]; then
+        echo $app is running...
+    fi
+  done 
+}
+
+function restart_app()
+{
+  echo ""
+  for app in "${APPLIST[@]}"; do
+    kill_bin $app
+    run_bin $app
+  done
+}
+
+function list_app()
+{
+  for app in "${APPLIST[@]}"; do
+    echo $app
+  done
+}
+
+function maintain_app()
+{
+  for app in "${APPLIST[@]}"; do
+    ps -ef | grep ${app} | grep -v "grep" >/dev/null 2>&1
+    if [[ ! $? -eq 0 ]]; then
+        run_bin ${app}
+    fi
+  done
+}
+while getopts "hsrlm" OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         s)
+            show_app
+             ;;
+         r)
+            restart_app
+            ;;
+         l)
+            list_app
+            ;;
+         m)
+            maintain_app
+            ;;
+         ?)
+             usage
+             exit 1
+             ;;
+     esac
 done
 
-echo "exit"
-exit
+if [[ $# -eq 0 ]]; then
+  echo "Maintain ..."
+  maintain_app
+fi
+
