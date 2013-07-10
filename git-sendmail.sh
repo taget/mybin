7fc9eec3be81b3a6597804a0d9157418e1773495
Eli Qiao <taget@linux.vnet.ibm.com>
2013-07-10 17:14:26 +0800

    ad download rpms from ftp
    Signed-off-by: Eli Qiao <taget@linux.vnet.ibm.com>

--- git-sendmail.sh
+++ git-sendmail.sh
@@ -5,7 +5,7 @@ echo $@
 git send-email --smtp-server ap.relay.ibm.com "$@" --from taget@linux.vnet.ibm.com --to frobisher@lists.linux.ibm.com
 
 
-#git send-email --smtp-server ap.relay.ibm.com "$@" --from taget@linux.vnet.ibm.com --to taget@linux.vnet.ibm.com
+#git send-email --smtp-server ap.relay.ibm.com --smtp-debug 1 "$@" --from taget@linux.vnet.ibm.com --to taget@linux.vnet.ibm.com
 
 
-#git send-email --smtp-server ap.relay.ibm.com "$1" --from taget@linux.vnet.ibm.com --to rhevh-blue@lists.linux.ibm.com
+#git send-email --smtp-server ap.relay.ibm.com --smtp-debug 1 "$@" --from taget@linux.vnet.ibm.com --to rhevh-blue@lists.linux.ibm.com --to frobisher@lists.linux.ibm.com
